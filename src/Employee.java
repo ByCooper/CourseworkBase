a58438b6fcd70884bdf5c4ca3007686b14b61945
ByCooper <kup.official@yahoo.com>
2023-04-20 14:42:29 +0300

    Курсовая работа (базовая сложность)(выполнено)

--- src/Employee.java
+++ src/Employee.java
@@ -1,3 +1,5 @@
+import java.text.DecimalFormat;
+
 public class Employee {
 
     String firstName;
@@ -58,5 +60,50 @@ public class Employee {
 
     public String getFirstName() {
         return firstName;
+    }
+
+    public static int allSalary(Employee[] o) {
+        int sum = 0;
+        int a = 0;
+        for (int i = 0; i <= o.length - 1; i++) {
+            a = o[i].salary;
+            sum = sum + a;
+        }
+        return sum;
+    }
+
+    public static void minSalary(Employee[] o) {
+        int min = o[0].salary;
+        Employee unitMin = o[0];
+        for (int i = 0; i <= o.length - 1; i++) {
+            if (min > o[i].salary) {
+                min = o[i].salary;
+                unitMin = o[i];
+            }
+        }
+        System.out.println("Минимальная зарплата по организации: " + "\n" + unitMin);
+    }
+
+    public static void maxSalary(Employee[] o) {
+        int max = o[0].salary;
+        Employee unitMax = o[0];
+        for (int i = 0; i <= o.length - 1; i++) {
+            if (max < o[i].salary) {
+                max = o[i].salary;
+                unitMax = o[i];
+            }
+        }
+        System.out.println("Максимальная зарплата по организации: " + "\n" + unitMax);
+    }
+
+    public static void averageSalary(Employee[] o, int i) {
+        double average = (double) i / o.length;
+        System.out.println("Среднее занчение зарплат равно " + new DecimalFormat("###,###.##").format(average));
+    }
+
+    public static void allName(Employee[] o) {
+        for (int i = 0; i <= o.length - 1; i++) {
+            System.out.println(o[i].middleName + " " + o[i].firstName + " " + o[i].lastName);
+        }
     }
 }
